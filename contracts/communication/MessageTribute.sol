9fd528855f0f8836718285a91909289f1cd2699f
Ricardo Guilherme Schmidt <ricardo3@status.im>
2019-06-14 06:53:18 -0300

    separated whisper key support

--- contracts/communication/MessageTribute.sol
+++ contracts/communication/MessageTribute.sol
@@ -1,21 +1,41 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "../common/Controlled.sol";
+import "../common/MessageSigned.sol";
 
 /**
  * @notice Defines tribute to talk
  */
-contract MessageTribute is Controlled {
-    event SetTribute(address indexed account, uint256 value);
+contract MessageTribute is Controlled, MessageSigned {
+    event SetTribute(address indexed account, address beneficiary, uint256 value);
     bool public stopped;
-    mapping(address => uint256) tributeCatalog;
+    struct Tribute {
+        address beneficiary;
+        uint256 value;
+    }
+    mapping(address => Tribute) tributeCatalog;
 
     /**
      * @notice Set tribute of account
      * @param _value Required tribute value
      */
-    function setTribute(uint256 _value) external {
-         setTribute(msg.sender, _value);
+    function setTribute(address _beneficiary, uint256 _value) external {
+         setTribute(msg.sender, _beneficiary, _value);
+    }
+
+    /**
+     * @notice Set tribute of account using signature
+     * @param _value Required tribute value
+     * @param _ttl TTL of message
+     * @param _messageSignature signature of hashTributeMessage(_value, _ttl)
+     */
+    function setTribute(address _beneficiary, uint256 _value, uint256 _ttl, bytes calldata _messageSignature) external {
+        uint256 time = block.timestamp;
+        require(time < _ttl && _ttl-time < 1 days, "Invalid TTL");
+        address signer = recoverAddress(getSignHash(hashTributeMessage(_beneficiary, _value, _ttl)), _messageSignature);
+        require(signer != address(0), "Invalid signer");
+        setTribute(signer, _beneficiary, _value);
+
     }
 
     /**
@@ -31,20 +51,29 @@ contract MessageTribute is Controlled {
      * @return value of tribute
      */
     function getTribute(address _of) external view
-        returns (uint256)
+        returns (address, uint256)
     {
-        return tributeCatalog[_of];
+        Tribute memory tribute = tributeCatalog[_of];
+        return (tribute.beneficiary, tribute.value);
     }
 
+    /**
+     * @notice generates hash for signing tributes
+     * @param _value Required tribute value
+     * @param _ttl TTL of message
+     */
+    function hashTributeMessage(address _beneficiary, uint256 _value, uint256 _ttl) public view returns(bytes32) {
+        return keccak256(abi.encodePacked(address(this), _beneficiary, _value, _ttl));
+    }
     /**
      * @notice Changes tribute of account
      * @param _of Account chaning tribute
      * @param _value New tribute value
      */
-    function setTribute(address _of, uint256 _value) internal {
+    function setTribute(address _of, address _beneficiary, uint256 _value) internal {
         require(!stopped, "Contract stopped by Controller");
-        tributeCatalog[_of] = _value;
-        emit SetTribute(_of, _value);
+        tributeCatalog[_of] = Tribute({beneficiary: _beneficiary, value: _value});
+        emit SetTribute(_of, _beneficiary, _value);
     }
 
 }
